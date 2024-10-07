9aaf3036c298a34567ed6dcb56f16fb29e6d0068
Arvin Garcia <arvin.garcia.dev@gmail.com>
2024-10-07 21:57:10 +0800

    Complete the seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  tempC = (tempF - 32) * 5 / 9
+  tempC = +tempC.toFixed(1);
+  return tempC
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  tempF= (tempC * 9 / 5) + 32
+  tempF = +tempF.toFixed(1);
+  return tempF
 };
 
 // Do not edit below this line
