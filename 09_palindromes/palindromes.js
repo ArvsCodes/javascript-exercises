7263ca7b04af2e805840862399296a04f6d1b391
Arvin Garcia <arvin.garcia.dev@gmail.com>
2024-10-08 16:07:00 +0800

    Complete the ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const validList = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const strNew = str.toLowerCase().split('').filter((character) => validList.includes(character)).join('');
+  
+    const strRev = strNew.split('').reverse().join('');
+  
+    return strNew === strRev;
 };
 
 // Do not edit below this line
